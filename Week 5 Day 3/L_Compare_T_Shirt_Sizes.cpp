0fb99581cada3cad957b5890d0304ad86c67ce5a
meem-5971 <105982702+meem-5971@users.noreply.github.com>
2023-10-31 23:23:06 +0600

    Add files via upload

---
+++ Week 5 Day 3/L_Compare_T_Shirt_Sizes.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+  
+int main(){
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+   int t; cin>>t;
+   while(t--){
+    string a,b;
+    cin>>a>>b;
+    if(a[a.size()-1]=='M' || b[b.size()-1]=='M'){
+            if(a[a.size()-1]=='L'){
+                cout<<">"<<endl;
+            }
+            else if(b[b.size()-1]=='L'){
+                cout<<"<"<<endl;
+            }
+            else if(a[a.size()-1]=='S'){
+                cout<<"<"<<endl;
+            }
+            else if(b[b.size()-1]=='S'){
+                cout<<">"<<endl;
+            }
+            else{
+                cout<<"="<<endl;
+            }
+        }
+        else{
+            if(a[a.size()-1]=='S'){
+                if(b[b.size()-1]=='S'){
+                    if(a.size()==b.size()){
+                        cout<<"="<<endl;
+                    }
+                    else if(a.size()>b.size()){
+                        cout<<"<"<<endl;
+                    }
+                    else{
+                        cout<<">"<<endl;
+                    }
+                }
+                else{
+                    cout<<"<"<<endl;
+                }
+            }
+            else{
+                if(b[b.size()-1]=='S'){
+                    cout<<">"<<endl;
+                }
+                else{
+                    if(a.size()==b.size()){
+                        cout<<"="<<endl;
+                    }
+                    else if(a.size()>b.size()){
+                        cout<<">"<<endl;
+                    }
+                    else{
+                        cout<<"<"<<endl;
+                    }
+                }
+            }
+        }
+   }
+    return 0;
+}
---
+++ Week 5 Day 3/M_Funny_Permutation.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+   int t;   cin>>t;
+   while(t--){
+    int n; cin>>n;
+    vector<int>v(n+1);
+   
+    if(n==3){
+        cout<<-1<<endl;
+        continue;
+    }
+    else if(n==5){
+        cout<<"5 4 1 2 3"<<endl;
+            continue;
+    }
+     for(int i=1;i<=n;i++){
+        v[i]=(n-i+1);
+    }
+   //reverse(v.begin(),v.end());
+    if(n%2 !=0){
+        swap(v[n/2],v[(n/2)+1]);
+         for(int i=1;i<=n;i++){
+        cout<<v[i]<<" ";
+    }
+        cout<<endl;
+    }
+    else{
+        for(int i=1;i<=n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    }
+   }
+}
