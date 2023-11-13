9de080b68ea42cc896fbbc771775a93468fa13a4
meem-5971 <105982702+meem-5971@users.noreply.github.com>
2023-11-13 23:51:10 +0600

    Add files via upload

---
+++ Week 7 Day 3/Four_Tickets.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+   int t; cin>>t;
+   while(t--){
+    int n;
+    cin>>n;
+    if((n*4)<=1000) cout<<"YES"<<'\n';
+    else cout<<"NO"<<'\n';
+   }
+    return 0;
+}
---
+++ Week 7 Day 3/Return_the_Change.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+  
+void solve(){
+        
+}
+  
+int main(){
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+   int t; cin>>t;
+   while(t--){
+    float n;
+    cin>>n;
+    float d= n/10;
+    cout<<100- round(d)*10<<endl;
+   }
+    return 0;
+}
